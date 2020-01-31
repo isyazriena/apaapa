0cbd9e08c7015b9f199c65f4f87ef94eafc2dc68
isyazriena <schazriena@gmail.com>
2020-01-31 09:23:22 +0800

    heeeeeeeeeeeeeehe

--- PMS/Admin/projectMembersAdmin.php
+++ PMS/Admin/projectMembersAdmin.php
@@ -145,7 +145,7 @@
 					<?php
 						echo "<select name='members[]' id='members1' style='display:block;'>";						
 								while ($row = mysqli_fetch_assoc($list)){
-									echo '<option value="'.$row['user.userId'].'">'.$row['user.userName'].'</option>';
+									echo '<option value="'.$row['userId'].'">'.$row['userName'].'</option>';
 								}
 						echo "</select>";
 					?>
